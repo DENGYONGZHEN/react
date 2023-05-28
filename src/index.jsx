dccbf1034dbed89baf914b390c5d1889eb6c8bff
DENGYONGZHEN <YONGZHENDENG111@gmail.com>
2023-05-28 20:00:16 +0900

    init

--- src/index.jsx
+++ src/index.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import '@/index.less';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  
     <div>学习，赚钱 </div>
-  
+    
 );
+
+fetch('/api/subscriptions/recommended_collections')
+  .then(response => response.json())
+  .then(value => {
+    console.log('成功:', value);
+  });
+
+
 
 
